b0be9e83eca4f8527b44ec32e397ee57a0a2ae24
ronald13130 <ronaldpino1313@gmail.com>
2024-07-19 17:13:45 -0500

    mostrar todos los datos actualizar//id no encontrado

--- app/src/main/java/com/example/interfazandroid/Usuario/EditarPerfilUsuario.java
+++ app/src/main/java/com/example/interfazandroid/Usuario/EditarPerfilUsuario.java
@@ -1,11 +1,13 @@
 package com.example.interfazandroid.Usuario;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,16 +18,178 @@ import androidx.core.view.WindowCompat;
 
 import com.example.interfazandroid.MenuRegistro.MainActivity;
 import com.example.interfazandroid.R;
+import com.example.interfazandroid.databinding.ActivityEditarPerfilUsuarioBinding;
+import com.example.interfazandroid.modelApi.ApiLogin;
+import com.example.interfazandroid.modelApi.ApiService;
+import com.example.interfazandroid.modelApi.UserDetails;
+import com.example.interfazandroid.modelApi.UserUpdate;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EditarPerfilUsuario extends AppCompatActivity {
+
+    private TextInputEditText edtNombre, edtApellido,edtEmail, edtNumIdentificacion,edtTelefono,edtNacimiento,edtCaracterizacion;
+    private Button btnguardardatos;
+    private ApiService apiService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_editar_perfil_usuario);
-        //////////////////////////////////TOOLBAR///////////////////////////////////////////////////
 
+        edtNombre = findViewById(R.id.edtNombre);
+        edtApellido = findViewById(R.id.edtApellido);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtNumIdentificacion = findViewById(R.id.edtNumIdentificacion);
+        edtTelefono = findViewById(R.id.edtTelefono);
+        edtNacimiento = findViewById(R.id.edtNacimiento);
+        edtCaracterizacion = findViewById(R.id.edtCaracterizacion);
+        btnguardardatos = findViewById(R.id.btnguardardatos);
+
+        MostrarDatos();
+        Toolbar();
+
+        btnguardardatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarDatosUsuario();
+            }
+        });
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+
+    private void MostrarDatos() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
+        String token = sharedPreferences.getString("UserToken", null);
+
+        if (token == null) {
+            Log.e("PerfilUsuario", "Token no encontrado");
+            Toast.makeText(this, "Token no encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.d("PerfilUsuario", "Token: " + token);
+        ApiService apiService = new ApiLogin().getRetrofitInstance().create(ApiService.class);
+        Call<UserDetails> call = apiService.getUserDetails("Bearer " + token);
+
+        call.enqueue(new Callback<UserDetails>() {
+            @Override
+            public void onResponse(Call<UserDetails> call, Response<UserDetails> response) {
+                Log.d("PerfilUsuario", "Código de respuesta: " + response.code());
+                if (response.isSuccessful()) {
+                    UserDetails userDetails = response.body();
+                    if (userDetails != null && userDetails.getSub() != null) {
+                        UserDetails.Sub sub = userDetails.getSub();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                edtNombre.setText(sub.getNombre());
+                                edtApellido.setText(sub.getApellido());
+                                edtEmail.setText(sub.getEmail());
+                                edtNumIdentificacion.setText(sub.getNumIdentificacion());
+                                edtTelefono.setText(sub.getTelefono());
+                                edtNacimiento.setText(sub.getFechaNacimieto());
+                                edtCaracterizacion.setText(sub.getCaracterizacion());
+                                Log.d("PerfilUsuario", "UI actualizada con los datos del usuario");
+                            }
+                        });
+                    } else {
+                        Log.e("PerfilUsuario", "UserDetails o Sub es null");
+                    }
+                } else {
+                    try {
+                        String errorBody = response.errorBody().string();
+                        Log.e("PerfilUsuario", "Error al obtener detalles del usuario. Código: " + response.code() + ", Cuerpo: " + errorBody);
+                        if (response.code() == 503) {
+                            Toast.makeText(EditarPerfilUsuario.this, "Servidor no disponible. Inténtalo más tarde.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(EditarPerfilUsuario.this, "Error al obtener detalles de usuario: " + response.code(), Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        Log.e("PerfilUsuario", "Error al leer el cuerpo del error", e);
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<UserDetails> call, Throwable t) {
+                Log.e("PerfilUsuario", "Error de conexión: " + t.getMessage(), t);
+                Toast.makeText(EditarPerfilUsuario.this, "Falló la conexión: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void actualizarDatosUsuario(){
+        String nombre = edtNombre.getText().toString();
+        String apellido = edtApellido.getText().toString();
+        String email = edtEmail.getText().toString();
+        String numIdentificacion = edtNumIdentificacion.getText().toString();
+        String telefono = edtTelefono.getText().toString();
+        String fechaNacimiento = edtNacimiento.getText().toString();
+        String caracterizacion = edtCaracterizacion.getText().toString();
+
+
+        //VERIFICAR LOS CAMPOS REQUERIDOS QUE ESTEN LLENOS
+        if(nombre.isEmpty() || apellido.isEmpty() || email.isEmpty()){
+            Toast.makeText(this,"Campos Requeridos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        UserUpdate updatedUserDetails = new UserUpdate(
+                nombre, apellido, email, numIdentificacion, telefono, fechaNacimiento, caracterizacion
+        );
+        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
+        String token = sharedPreferences.getString("UserToken", null);
+        String userId = sharedPreferences.getString("UserId", null);
+
+        if (token == null) {
+            Log.e("EditarPerfilUsuario", "Token no encontrado");
+            Toast.makeText(this, "Token no encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userId == null) {
+            Log.e("EditarPerfilUsuario", "UserId no encontrado");
+            Toast.makeText(this, "UserId no encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        ApiService apiService = new ApiLogin().getRetrofitInstance().create(ApiService.class);
+        Call<Void> call = apiService.updateUserProfile(userId, "Bearer " + token, updatedUserDetails);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(EditarPerfilUsuario.this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        String errorBody = response.errorBody().string();
+                        Log.e("EditarPerfilUsuario", "Error al actualizar datos del usuario. Código: " + response.code() + ", Cuerpo: " + errorBody);
+                        Toast.makeText(EditarPerfilUsuario.this, "Error al actualizar datos: " + response.code(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        Log.e("EditarPerfilUsuario", "Error al leer el cuerpo del error", e);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("EditarPerfilUsuario", "Error de conexión: " + t.getMessage(), t);
+                Toast.makeText(EditarPerfilUsuario.this, "Falló la conexión: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    private void Toolbar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ImageView icon1 = findViewById(R.id.icon1);
@@ -35,7 +199,6 @@ public class EditarPerfilUsuario extends AppCompatActivity {
         icon1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navega a la vista deseada para icon1
                 Intent intent = new Intent(EditarPerfilUsuario.this, PerfilUsuario.class);
                 startActivity(intent);
             }
@@ -43,7 +206,6 @@ public class EditarPerfilUsuario extends AppCompatActivity {
         icon2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navega a la vista deseada para icon2
                 Intent intent = new Intent(EditarPerfilUsuario.this, MenuUsuario.class);
                 startActivity(intent);
             }
@@ -51,25 +213,13 @@ public class EditarPerfilUsuario extends AppCompatActivity {
         icon3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navega a la vista deseada para icon3
                 Intent intent = new Intent(EditarPerfilUsuario.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-        ////////////////////////////////////////////////////////////////////////////////////////////
 
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-
-        Button btnCrear = findViewById(R.id.btnguardardatos);
-
-        btnCrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(EditarPerfilUsuario.this, "Guardado exitosamente", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
+
 }
 
 
--- app/src/main/java/com/example/interfazandroid/modelApi/ApiService.java
+++ app/src/main/java/com/example/interfazandroid/modelApi/ApiService.java
@@ -5,8 +5,12 @@ import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface ApiService {
     @FormUrlEncoded
     @POST("auth/login")
@@ -19,4 +23,12 @@ public interface ApiService {
 
     @GET("auth/profile")
     Call<UserDetails> getUserDetails(@Header("Authorization") String token);
+
+    @HTTP(method = "PATCH", path = "auth/usuario/{userId}", hasBody = true)
+    Call<Void> updateUserProfile(
+            @Path("_id") String userId,
+            @Header("Authorization") String token,
+            @Body UserUpdate userUpdateRequest
+    );
+
 }
