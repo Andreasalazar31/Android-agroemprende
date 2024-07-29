382f60b36ab6d31056e51bc5d75bff32f32c5882
Andreasalazar31 <andreayuliethsalazarperez31@gmail.com>
2024-07-29 14:05:12 -0500

    vista de registro

--- app/src/main/java/com/example/interfazandroid/Usuario/NotificacionesUsuario.java
+++ app/src/main/java/com/example/interfazandroid/Usuario/NotificacionesUsuario.java
@@ -19,74 +19,8 @@ public class NotificacionesUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_notificaciones_usuario);
+        //  setContentView(R.layout.activity_notificaciones_usuario);
         //////////////////////////////////TOOLBAR///////////////////////////////////////////////////
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ImageView icon1 = findViewById(R.id.icon1);
-        ImageView icon2 = findViewById(R.id.icon2);
-        ImageView icon3 = findViewById(R.id.icon3);
-
-        icon1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Navega a la vista deseada para icon1
-                Intent intent = new Intent(NotificacionesUsuario.this, PerfilUsuario.class);
-                startActivity(intent);
-            }
-        });
-        icon2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Navega a la vista deseada para icon2
-                Intent intent = new Intent(NotificacionesUsuario.this, MenuUsuario.class);
-                startActivity(intent);
-            }
-        });
-        icon3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Navega a la vista deseada para icon3
-                Intent intent = new Intent(NotificacionesUsuario.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        ////////////////////////////////////////////////////////////////////////////////////////////
-
-        Button btn1 = findViewById(R.id.notification_button);
-        Button btn2 = findViewById(R.id.notification_butto);
-        Button btn3 = findViewById(R.id.notification_butt);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                abrirActividadDetalles("Detalles del primer proyecto");
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                abrirActividadDetalles("Detalles del segundo proyecto");
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                abrirActividadDetalles("Detalles del tercer proyecto");
-            }
-        });
     }
-
-    private void abrirActividadDetalles(String mensaje) {
-        // Aquí configuras la actividad a la que quieres navegar
-        Intent intent = new Intent(NotificacionesUsuario.this, DetallesProyecto.class);
-        intent.putExtra("mensaje", mensaje); // Puedes enviar datos extras si es necesario
-        startActivity(intent);
-    }
-}
+};
