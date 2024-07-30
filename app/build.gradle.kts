75cf4cec5be1f5e9e515f8e196fc4669cd153a8f
Andreasalazar31 <andreayuliethsalazarperez31@gmail.com>
2024-07-30 18:11:18 -0500

    falta menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     //DEPENDENCIAS AGREGADAS
+    implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
--- app/src/main/java/com/example/interfazandroid/UsuarioMenu.java
+++ app/src/main/java/com/example/interfazandroid/UsuarioMenu.java
@@ -19,37 +19,5 @@ public class UsuarioMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_usuario_menu);
-        Toolbar();
-
-    }
-    private void Toolbar(){
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ImageView icon1 = findViewById(R.id.icon1);
-        ImageView icon3 = findViewById(R.id.icon3);
-
-        icon1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UsuarioMenu.this, UsuarioPerfil.class);
-                startActivity(intent);
-            }
-        });
-
-        icon3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clearSharedPreferences();
-                Intent intent = new Intent(UsuarioMenu.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-    }
-    private void clearSharedPreferences() {
-        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear(); // Borra todas las preferencias
-        editor.apply();
     }
 }
