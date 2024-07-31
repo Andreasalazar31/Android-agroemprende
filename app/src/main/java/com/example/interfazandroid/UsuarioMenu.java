4175a9cf8cc3b5a0420f27f4416470cf0ac60a47
vargas023 <alejandravargasb1@gmail.com>
2024-07-31 13:44:04 -0500

    Menu ya con sus vistas

--- app/src/main/java/com/example/interfazandroid/UsuarioMenu.java
+++ app/src/main/java/com/example/interfazandroid/UsuarioMenu.java
@@ -3,21 +3,57 @@ package com.example.interfazandroid;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.interfazandroid.MenuRegistro.MainActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UsuarioMenu extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_usuario_menu);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    // Ya estamos en Home, no necesitamos hacer nada
+                    return true;
+                } else if (itemId == R.id.projects) {
+                    startActivity(new Intent(UsuarioMenu.this, UsuarioNuevoProyecto.class));
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    startActivity(new Intent(UsuarioMenu.this, UsuarioPerfil.class));
+
+                    return true;
+                } else if (itemId == R.id.cerrarSesion) {
+                    clearSharedPreferences();
+                    startActivity(new Intent(UsuarioMenu.this, MainActivity.class));
+                    // Implementa la lógica para cerrar sesión aquí
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+    private void clearSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear(); // Borra todas las preferencias
+        editor.apply();
+    }
+
 }
